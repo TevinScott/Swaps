271c15fd017fb8cb9602a1f06aceb77d36b25d6f
Hey-Scotty <tinytevin23@icloud.com>
2018-01-15 11:40:41 -0500

    added Basic Pin Drop functionality
    user can place pin on map if they long press a location.

--- Swaps/PickupVC.swift
+++ Swaps/PickupVC.swift
@@ -32,9 +32,19 @@ class PickupVC : UIViewController, CLLocationManagerDelegate{
         self.locationManager.startUpdatingLocation()
         panToCurrentLocation()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        panToCurrentLocation()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
     
     // MARK: - Button Actions
-    
+
     /**
      This Button, when pressed will either cause the mapView to stay centered on the user's location or stop the fixed centering
     */
@@ -46,7 +56,6 @@ class PickupVC : UIViewController, CLLocationManagerDelegate{
             toggleFollowBtn.imageView?.image = UIImage(named: "currentLocationPanningDisabled")
             mapView.setUserTrackingMode(MKUserTrackingMode.none, animated: true)
         }
-        
     }
     
     /**
@@ -55,27 +64,23 @@ class PickupVC : UIViewController, CLLocationManagerDelegate{
     @IBAction func meetupBtnPressed(_ sender: Any) {
         saleItem.pickupLocation = (Double((locationManager.location?.coordinate.latitude)!),
                                    Double((locationManager.location?.coordinate.longitude)!))
-        //NEEDS: Dialog Btn
+        //NEEDS: Dialog Box & Location needs to match the annotation/pin placed by user
         algoliaHandle.addPickupLocationToItem(saleItem: saleItem)
     }
     
-    
-    // MARK: - Support Functions
     /**
-     called once user allows location permissions. will move the map view to the user's current location should the move.
+     adds a pin to the location the user last tapped on inside this class's mapView
     */
-    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //should really only center once when the view is onpened, this locks the mapview to where the user is currently located
-        if(mapView.userTrackingMode.rawValue == 0){
-            if(!(toggleFollowBtn.imageView?.image?.isEqual(UIImage(named: "currentLocationPanningDisabled")))!) {
-                toggleFollowBtn.imageView?.image = UIImage(named: "currentLocationPanningDisabled")
-            }
-            
-        } else {
-            if(!(toggleFollowBtn.imageView?.image?.isEqual(UIImage(named: "currentLocationPanningEnabled")))!) {
-                toggleFollowBtn.imageView?.image = UIImage(named: "currentLocationPanningEnabled")
-            }
-        }
+    @IBAction func addPin(_ sender: UILongPressGestureRecognizer) {
+        let location = sender.location(in: mapView)
+        let locCoord = mapView.convert(location, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locCoord
+        annotation.title = "Meet Up Location"
+        annotation.subtitle = "Initial Request"
+        print("pinFunction Called")
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(annotation)
     }
     
     /**
@@ -84,8 +89,26 @@ class PickupVC : UIViewController, CLLocationManagerDelegate{
     private func panToCurrentLocation(){
         let center = CLLocationCoordinate2D(latitude: (locationManager.location?.coordinate.latitude)!, longitude: (locationManager.location?.coordinate.longitude)!)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-        self.mapView.setRegion(region, animated: true)
-        self.mapView.showsUserLocation = true
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
     }
-  
+    
+    // MARK: - Support Functions
+    /**
+     called once user allows location permissions. will move the map view to the user's current location should the move.
+     */
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //should really only center once when the view is onpened, this locks the mapview to where the user is currently located
+        if(mapView.userTrackingMode.rawValue == 0){
+            if(!(toggleFollowBtn.imageView?.image?.isEqual(UIImage(named: "currentLocationPanningDisabled")))!) {
+                toggleFollowBtn.imageView?.image = UIImage(named: "currentLocationPanningDisabled")
+            }
+        } else {
+            if(!(toggleFollowBtn.imageView?.image?.isEqual(UIImage(named: "currentLocationPanningEnabled")))!) {
+                toggleFollowBtn.imageView?.image = UIImage(named: "currentLocationPanningEnabled")
+            }
+        }
+    }
+    
+
 }
